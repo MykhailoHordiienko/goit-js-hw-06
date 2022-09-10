60882240fc4f699d3be66c326d77a13591706f00
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-09-10 19:02:27 +0300

    final HW6

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Заполните поля!");
+  }
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+  formRef.reset();
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesRef = document.querySelector("#boxes");
+const createBtnRef = document.querySelector("button[data-create]");
+const destroyBtnRef = document.querySelector("button[data-destroy]");
+const inputRef = document.querySelector("input");
+
+let number = 0;
+inputRef.addEventListener("input", changeNumber);
+function changeNumber(event) {
+  number = Number(event.target.value);
+}
+
+createBtnRef.addEventListener("click", onCreateBtnClick);
+
+function onCreateBtnClick() {
+  for (let i = 0; i < number; i++) {
+    let num = 30;
+    const newBox = `<div style="width: ${num + i * 10}px; height: ${
+      num + i * 10
+    }px; background-color: ${getRandomHexColor()};"></div>`;
+
+    boxesRef.insertAdjacentHTML("afterbegin", newBox);
+  }
+}
+
+destroyBtnRef.addEventListener("click", () => {
+  boxesRef.innerHTML = "";
+  inputRef.value = "";
+  number = 0;
+});
